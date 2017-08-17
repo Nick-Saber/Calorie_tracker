90296039fc101e4425a666de280a7594012e1136
Nicholas Saber <nsaber@berkeley.edu>
2017-08-17 11:47:27 -0700

    Replaced direct SQL Queries in psycopg2 by using SQLAlchemy and wrote respective api calls for plotting functionality

--- website/app.py
+++ website/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect, session
 import psycopg2 as psql
 from psycopg2.extensions import AsIs, QuotedString
 import json
-import bleach
+import db_api
 
 
 # from flask.ext.sqlalchemy import SQLAlchemy
@@ -29,55 +29,41 @@ app = Flask(__name__)
 
 
 
-db_name='calorie_tracker'
-
-def add_food_entry(food,caloric_value,user):
-	db=psql.connect(dbname=db_name)
-	curs=db.cursor()
-	curs.execute('INSERT INTO entries (food_name, calories, user_name) VALUES (%s,%s,%s);', (bleach.clean(food),caloric_value,bleach.clean(user)))
-	db.commit()
-	db.close()
-
-def get_todays_entries(user):
-	db=psql.connect(dbname=db_name)
-	curs=db.cursor()
-	curs.execute('SELECT entries.food_name, entries.calories FROM entries WHERE entries.user_name=%s and entries.day=current_date;', (bleach.clean(user),))
-	entries=[{'food':str(row[0]),'calories':int(row[1]) } for row in curs.fetchall()]
-	db.close()
-	return entries
-
-def get_caloric_consumption(user,date=None):
-	db=psql.connect(dbname=db_name)
-	curs=db.cursor()
-	if date == None:
-		curs.execute("""SELECT entries.food_name, entries.calories FROM entries WHERE entries.user_name=%s and entries.day=current_date;""", (bleach.clean(user),))
-	else:
-		curs.execute("""SELECT entries.food_name, entries.calories FROM entries WHERE entries.user_name=%s and entries.day=date;""", (bleach.clean(user),bleach.clean(date),))
-
-	entries=[{'food':str(row[0]),'calories':int(row[1]) } for row in curs.fetchall()]
-	db.close()
-	consumption=0
-	for item in entries:
-		consumption=consumption + item['calories']
-	return consumption
-
-def get_past_x_days_cc(user,days=7):
-	db=psql.connect(dbname=db_name)
-	curs=db.cursor()
-	curs.execute("""SELECT SUM(entries.calories), entries.day FROM entries WHERE entries.user_name=%s GROUP BY entries.day ORDER BY entries.day DESC;""", (bleach.clean(user),))
-	entries=[row for row in curs.fetchmany(days)]
-	db.close()
-	return entries
-
-
-
-
-
-@app.route('/' methods= ['GET','POST'])
+@app.route('/',  methods= ['GET','POST'])
 def login():
 	dict_variables={}
-	dict_variables['user']=session.get('username')
-	return render_template('login.html' , dict_variables=dict_variables)
+
+	#Process Logging In
+	if request.method=='POST':
+		#Account creation
+		if request.form.get('login_type')=='Create':
+			if (request.form.get('password1')==request.form.get('password2')) and (request.form.get('user_name')!= None):
+				if db_api.add_user(request.form.get('user_name'),request.form.get('password1')):
+					session['username']=request.form.get('user_name')
+					dict_variables['user']=session.get('username')
+					return render_template('entry_page.html',dict_variables=dict_variables)
+				else:
+					dict_variables['create_acc_error_message']='Error with account creation, Invalid Credentials'
+					return render_template('login.html',dict_variables=dict_variables)
+			else:
+				dict_variables['create_acc_error_message']='Error with account creation, Passwords don\'t match'
+				return render_template('login.html',dict_variables=dict_variables)
+
+		#Logging in to an existing account
+		if request.form.get('login_type')=='Login':
+			result= db_api.check_login(request.form.get('user_name_login'),request.form.get('login_password'))
+			if result[0]:
+				session['username']=request.form.get('user_name')
+				dict_variables['username']=session['username']
+				return render_template('entry_page.html',dict_variables=dict_variables)
+			else:
+				dict_variables['login_acc_err_message']=result[1]
+				return render_template('login.html',dict_variables=dict_variables)
+
+
+	if request.method=='GET':		
+		dict_variables['user']=session.get('username')
+		return render_template('login.html' , dict_variables=dict_variables)
 
 
 
@@ -102,19 +88,17 @@ def entry_page():
 			dict_variables['user']=session.get('username')
 			dict_variables['display_message']=True
 			dict_variables['message']="last entry was food: " +request.form['food_item'] +"  calories: " + request.form['caloric_value']
-			add_food_entry(request.form['food_item'],request.form['caloric_value'],dict_variables['user'])
+			db_api.add_entry(dict_variables['user'], request.form['food_item'],request.form['caloric_value'])
 
 
 			#list out and display users past entries and related info
-			dict_variables['entries']=get_todays_entries(dict_variables['user'])
-			dict_variables['display_entries']=True
-			dict_variables['consumption']=get_caloric_consumption(dict_variables['user'])
+			dict_variables['consumption']=db_api.get_todays_entries(dict_variables['user'])
 
 			#plot users past history data
-			history_data=get_past_x_days_cc(user=dict_variables['user'])
-			dict_variables['caloric_history_values']=json.dumps(reverse([entry[0] for entry in history_data])
-			dict_variables['caloric_history_dates']=json.dumps([entry[1].__str__() for entry in history_data ])
-			dict_variables['history_length'] = len(history_data)
+			history_data=db_api.get_past_x_days(dict_variables['user'],5)
+			dict_variables['caloric_history_values']=json.dumps(history_data[0])
+			dict_variables['caloric_history_dates']=json.dumps([entry.date().__str__() for entry in history_data[1]])
+			dict_variables['history_length'] = len(history_data[0])
 
 
 			return render_template('entry_page.html', dict_variables=dict_variables)
@@ -128,5 +112,6 @@ def entry_page():
 
 
 if __name__ == '__main__':
+
 	app.secret_key = 'some secret key'
 	app.run(host='0.0.0.0', port = 8091)
---
+++ website/db_api.py
@@ -0,0 +1,102 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData, Column, Table, ForeignKey, func
+from sqlalchemy import Integer, String, DateTime, cast, Date
+from sqlalchemy.orm import sessionmaker as sm 
+from sqlalchemy.schema import Sequence
+from sqlalchemy.sql import label
+import datetime
+
+engine = create_engine("postgresql://NickAccount:@localhost/calorie_tracker",echo=True)
+
+Base=declarative_base()
+db_session=sm(bind=engine)
+
+class user(Base):
+	__tablename__='users'
+
+	username=Column(String, primary_key=True)
+	password=Column(String)
+
+	def __init__(self, username,password):
+		self.username=username
+		self.password=password
+
+class food_entries(Base):
+	__tablename__='food_entries'
+
+	entry_id=Column('entry_id',Integer,Sequence("food_entries_sequence", start=1,increment=1),primary_key=True)
+	foodname=Column(String)
+	caloric_value=Column(Integer)
+	entry_date=Column(DateTime, default = func.current_timestamp())
+	username=Column(String, ForeignKey(user.username))
+
+	def __init__(self,foodname,caloric_value,username):
+		self.foodname=foodname
+		self.caloric_value=caloric_value
+		self.username=username
+
+
+Base.metadata.create_all(engine)
+
+#Functin that checks if a username exists and if so if the password is correct
+def check_login(login_name,pswd):
+	current_sesh = db_session()
+	result = current_sesh.query(user).get(str(login_name))
+	current_sesh.close()
+	if(result):
+		if(result.password==pswd):
+			return (True, "Login Succesful")
+		else:
+			return (False, "Password is incorrect")
+	else:
+		return (False, ("User %s does not exist Please Make an account" % login_name))
+
+
+#Adds new users to the database
+def add_user(username,password):
+	current_sesh = db_session()
+	current_sesh.add(user(username,password))
+	try:
+		current_sesh.commit()
+		committed=True
+	except:
+		committed=False
+	current_sesh.close()
+	return committed
+
+#adds new food entries to the database
+def add_entry(user, foodname,calories):
+	current_sesh = db_session()
+	current_sesh.add(food_entries(foodname,calories,user))
+	try:
+		current_sesh.commit()
+		committed=True
+	except:
+		committed=False
+	current_sesh.close()
+	return committed
+
+#Gets all the entries a user input today
+def get_todays_entries(user):
+	current_sesh = db_session()
+	result = current_sesh.query(food_entries).filter(food_entries.username =='{}'.format(user)).filter(cast(food_entries.entry_date,Date) == datetime.datetime.today().date())
+	sum=0
+	#food_entries.entry_date.date() == datetime.datetime.today().date(), 
+	for entry in result.all():
+		print(str(entry.entry_date.date()== datetime.datetime.today().date()))
+		sum+= entry.caloric_value
+	current_sesh.close()
+	return sum
+
+
+def get_past_x_days(user,x):
+	current_sesh=db_session()
+	consumption=current_sesh.query(food_entries.entry_date,label('caloric_consumption',func.sum(food_entries.caloric_value))).group_by(food_entries.entry_date).limit(x)
+	calories=[]
+	dates=[]
+	for total in consumption:
+		calories.append(total.caloric_consumption)
+		dates.append(total.entry_date)
+	return [calories,dates]
+	
